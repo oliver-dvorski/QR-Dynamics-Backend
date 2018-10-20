620907d4596c3e9034a07dbfce2cb031382435d9
oliver-dvorski <oliver.dvorski@gmail.com>
2018-10-20 22:00:48 +0200

    Spice up the welcome view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,1 +1,38 @@
-QR Dynamics
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>{{ config('app.name') }}</title>
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css?family=Cabin:700" rel="stylesheet">
+
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        }
+        body div {
+            height: 100vh;
+            width: 100vw;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            font-family: 'Cabin', sans-serif;
+            background: #f8fafc;
+        }
+        h1 {
+            font-weight: 700;
+            color: #1eaaf1;
+            font-size: 3rem;
+        }
+    </style>
+</head>
+<body>
+
+    <div>
+        <h1>QR Dynamics</h1>
+    </div>
+
+</body>
+</html>
